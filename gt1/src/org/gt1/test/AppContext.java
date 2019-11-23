cbb928c9e9aea540feab5cbb7ef55135722ce081
baichuan <baichuan@126.com>
2019-11-23 11:59:25 +0800

    zzz

--- gt1/src/org/gt1/test/AppContext.java
+++ gt1/src/org/gt1/test/AppContext.java
@@ -4,7 +4,13 @@ public class AppContext {
 
 	
 	public static void main(String[] args) {
-		System.out.println("welcome");
+
+		for (int i = 0; i < 10; i++) {
+			
+			System.out.println("ok");
+		}
+		
+	
 	}
 	
 }
